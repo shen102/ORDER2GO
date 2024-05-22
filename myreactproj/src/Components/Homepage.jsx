f864316f4e5e5d5ea66c64e1657fcb9cc34e7bf5
Cham2426 <145363346+CharmieValerieMontes@users.noreply.github.com>
2024-05-22 18:04:27 +0800

    ...

--- myreactproj/src/Components/Homepage.jsx
+++ myreactproj/src/Components/Homepage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './homepage.css';
-import coffeeIcon from "../images/Coffee.png";
+import coffeeIcon from '../images/Coffee.png';
 import milkTeaIcon from '../images/Milktea.png';
+import coffee from '../images/Ice Coffee.png';
+import okinawa from '../images/Okinawa.png';
 import searchIcon from '../icons/search.png';
 import cartIcon from '../icons/cart.png';
 import userIcon from '../icons/profile.png';
 import homeIcon from '../icons/home.png';
-import settingsIcon from '../icons/settings.png';
-import shopIcon from '../icons/shop.png';
+import promo from '../icons/promo.png';
+import transact from '../icons/transact.png';
+import BigBrew from '../images/BigBrew.png';
+import DonMacchiatos from '../images/Don Macchiatos.png';
+import Baabaa from '../images/Baa Baa.png';
 
 function Homepage() {
   return (
@@ -34,32 +39,35 @@ function Homepage() {
       <div className="top-products">
         <h2>Top Products</h2>
         <button className="product-button">
-          <img src={coffeeIcon} alt="Big Brew" />
+          <img src={okinawa} alt="Big Brew" />
           Big Brew
         </button>
         <button className="product-button">
-          <img src={milkTeaIcon} alt="Don Maka" />
+          <img src={coffee} alt="Don Maka" />
           Don Maka
         </button>
       </div>
       <div className="affiliate-shops">
         <h2>Affiliate Shop</h2>
         <button className="shop-button">
-          <img src={shopIcon} alt="Shop" />
+          <img src={BigBrew} alt="Shop" />
         </button>
         <button className="shop-button">
-          <img src={shopIcon} alt="Shop" />
+          <img src={DonMacchiatos} alt="Shop" />
         </button>
         <button className="shop-button">
-          <img src={shopIcon} alt="Shop" />
+          <img src={Baabaa} alt="Shop" />
         </button>
       </div>
       <div className="footer">
         <button className="footer-button">
+          <img src={promo} alt="Promo" />
+        </button>
+        <button className="footer-button">
           <img src={homeIcon} alt="Home" />
         </button>
         <button className="footer-button">
-          <img src={settingsIcon} alt="Settings" />
+          <img src={transact} alt="Transact" />
         </button>
       </div>
     </div>
