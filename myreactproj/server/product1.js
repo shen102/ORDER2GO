f24079f42b9d30681e6201dc577e5db3d2476723
Cham2426 <145363346+CharmieValerieMontes@users.noreply.github.com>
2024-05-28 09:45:11 +0800

    ......

---
+++ myreactproj/server/product1.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+      id: 1,
+      name: "Iced Caramel Macchiato",
+      brand: "Don Macchiatos",
+      desc: "venti",
+      price: 39,
+      image:
+        "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716859705/ICM_usspzu.png",
+    },
+    {
+      id: 2,
+      name: "Don Matchatos",
+      brand: "Don Macchiatos",
+      desc: "venti",
+      price: 39,
+      image:
+        "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716859705/DM_wus9el.png",
+    },
+    {
+      id: 3,
+      name: "Donya Berry",
+      brand: "Don Macchiatos",
+      desc: "Venti",
+      price: 39,
+      image:
+        "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716859705/DB_dz4gld.png",
+    },
+
+    {
+      id: 4,
+      name: "Don Darko",
+      brand: "Don Macchiatos",
+      desc: "Venti",
+      price: 39,
+      image:
+        "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716859705/DD_g8mrid.png",
+    },
+    {
+      id: 5,
+      name: "Matcha Berry",
+      brand: "Don Macchiatos",
+      desc: "Venti",
+      price: 39,
+      image:
+        "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716859705/MB_amkzjj.png",
+    },
+    {
+      id: 6,
+      name: "Black Florest",
+      brand: "Don Macchiatos",
+      desc: "Venti",
+      price: 39,
+      image:
+        "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716859705/BF_ntyk8g.png",
+    },
+  ];
+  
+  module.exports = products;
