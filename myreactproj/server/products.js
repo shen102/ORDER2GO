01e16b4f0933a0b5e937b825ab3070ac66bd9e73
shen102 <sheenrodrigazo@gmail.com>
2024-05-28 12:06:55 +0800

    ....

--- myreactproj/server/products.js
+++ myreactproj/server/products.js
@@ -36,6 +36,64 @@ const products = [
       image:
         "https://res.cloudinary.com/daql8nqvr/image/upload/v1716736788/Chocolate_andjx3.png",
     },
-  ];
   
-  module.exports = products;
+      {
+        id: 4,
+        name: "Banoffee Frappe",
+        brand: "Baa Baa",
+        desc: "venti",
+        price: 39,
+        image:
+          "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716860409/b1_t9jxmt.png",
+      },
+      {
+        id: 5,
+        name: "Cheese Cake Milktea",
+        brand: "Baa Baa",
+        desc: "venti",
+        price: 39,
+        image:
+          "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716860404/b2_oilyh9.png",
+      },
+      {
+        id: 6,
+        name: "Choco Thai Green Milktea",
+        brand: "Baa Baa",
+        desc: "Venti",
+        price: 39,
+        image:
+          "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716860405/b3_lnjhqh.png",
+      },
+  
+      {
+        id: 7,
+        name: "Chocolate Frappe",
+        brand: "Baa Baa",
+        desc: "Venti",
+        price: 39,
+        image:
+          "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716860406/b4_vgox0c.png",
+      },
+      {
+        id: 8,
+        name: "Cocoa Milk",
+        brand: "Baa Baa",
+        desc: "Venti",
+        price: 39,
+        image:
+          "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716860405/b5_o2zt2k.png",
+      },
+      {
+        id: 9,
+        name: "Stawberry Frappe",
+        brand: "Baa Baa",
+        desc: "Venti",
+        price: 39,
+        image:
+          "https://res.cloudinary.com/dn5ixb5lq/image/upload/v1716860407/b6_r0zk7s.png",
+      },
+    ];
+    
+    module.exports = products;
+  
+  
