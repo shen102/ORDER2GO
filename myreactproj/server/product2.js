91ed06fe151256891ea89faea77c498194e8ebba
Cham2426 <145363346+CharmieValerieMontes@users.noreply.github.com>
2024-05-28 21:26:28 +0800

    .....

--- myreactproj/server/product2.js
+++ myreactproj/server/product2.js
@@ -56,4 +56,4 @@ const products = [
     },
   ];
   
-  module.exports = products;
+  module.exports = products1;
--- myreactproj/src/Components/Receipt.jsx
+++ myreactproj/src/Components/Receipt.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import "./Receipt.css";
 import { useSelector } from "react-redux";
 
 const Receipt = () => {
   const cart = useSelector((state) => state.cart);
+  const App = () => {
+    const orderNumber = 46; // Example order number
+  
+    return (
+      <Provider store={store}>
+        <Receipt orderNumber={orderNumber} />
+      </Provider>
+    );
+  };
+  
+  ReactDOM.redux(<App />, document.getElementById("root"));
 
   return (
     <div className="receipt-container">
@@ -12,15 +24,15 @@ const Receipt = () => {
           <div key={item.id} className="receipt-item">
             <p>{item.name}</p>
             <p>Quantity: {item.cartQuantity}</p>
-            <p>Price: ${item.price}</p>
-            <p>Total: ${item.price * item.cartQuantity}</p>
+            <p>Price: ₱{item.price}</p>
+            <p>Total: ₱{item.price * item.cartQuantity}</p>
           </div>
         ))}
       </div>
       <div className="receipt-summary">
-        <p>Subtotal: ${cart.cartTotalAmount}</p>
-        <p>Taxes and Shipping: To be calculated</p>
-        <p>Total Amount: ${cart.cartTotalAmount}</p>
+        <p1>Subtotal: ₱{cart.cartTotalAmount}</p1>
+        <p1>Taxes and Shipping: To be calculated</p1>
+        <p1>Total Amount: ₱{cart.cartTotalAmount}</p1>
       </div>
     </div>
   );
